ee7231725c3bda1bd039b22ce7a46a0176b4b450
Roman Braunstingl <r.braunstingl@celix.at>
2025-06-30 19:31:58 +0200

    fix solution generation

--- min-dominating-set.py
+++ min-dominating-set.py
@@ -1,7 +1,7 @@
 import networkx as nx
 from networkx.algorithms import approximation
 
-path_to_gr_file = 'samples/C5.gr'
+path_to_gr_file = 'samples/two-levels.gr'
 
 
 def load_gr_file(filepath):
@@ -23,6 +23,26 @@ def load_gr_file(filepath):
 
 G = load_gr_file(path_to_gr_file)
 print("input parsed...", G)
+number_of_nodes = len(G.nodes)
+adjacency_matrix = []
+for i in range(number_of_nodes):
+    adjacency_matrix.append([])
+    for j in range(number_of_nodes):
+        adjacency_matrix[i].append(False)
+
+node_to_adjacency_index = {}
+counter = 0
+for node in G.nodes:
+    node_to_adjacency_index[node] = counter
+    counter += 1
+
+for u, v in G.edges:
+    index_u = node_to_adjacency_index[u]
+    index_v = node_to_adjacency_index[v]
+
+    adjacency_matrix[index_u][index_v] = True
+    adjacency_matrix[index_v][index_u] = True
+
 
 # get tree decomposition
 tree_width, decomposition = approximation.treewidth_min_fill_in(G)
@@ -76,27 +96,80 @@ def permutations(list_of_lists):
             yield [e] + g
 
 
-# calculate the possible solutions taking into account the visited neighbours given
+def has_edge(u, v):
+    index_u = node_to_adjacency_index[u]
+    index_v = node_to_adjacency_index[v]
+    return adjacency_matrix[index_u][index_v] == True
+
+
+def has_neighbour_in(v, possible_solution):
+    for other in possible_solution:
+        if has_edge(v, other):
+            return True
+
+    return False
+
+
+# a solution is valid if every node is either in the possible solution or is dominated (has a neighbour in) it
+def is_valid_solution(possible_solution, node):
+    for vertex in node:
+        if vertex in possible_solution:
+            continue
+
+        if not has_neighbour_in(vertex, possible_solution):
+            return False
+
+    return True
+
+
+def powerset(collection):
+    current_element = collection[0]
+    yield [current_element]
+    if len(collection) == 1:
+        return []
+
+    for subset in powerset(collection[1:]):
+        yield [current_element] + subset
+        yield subset
+
+
 def visit_node(node, list_of_visited_neighbours):
-    visited_nodes_to_statistics_set[node] = {}
-    for selected_vertex in node:  # nodes are sets of vertices (bags)
-        min_selected_set = set()
-        min_selected_set.add(selected_vertex)
-        min_selected_set_length = 0
-        for permutation in permutations(list_of_visited_neighbours):
-            selected_set = set()
-            selected_set.add(selected_vertex)
-            for i, vertex in enumerate(permutation):
-                current_neighbour = list_of_visited_neighbours[i]
-                for u in visited_nodes_to_statistics_set[current_neighbour][vertex]:
-                    selected_set.add(u)
+    visited_nodes_to_statistics_set[node] = {}  # map of lengths to solutions
 
-            current_length = len(selected_set)
-            if min_selected_set_length == 0 or current_length < min_selected_set_length:
-                min_selected_set = selected_set
-                min_selected_set_length = current_length
+    # map list of visited neighbours to their minimal solutions
+    minimal_solutions_per_neighbour = []
+    for neighbour in list_of_visited_neighbours:
+        solution_len_dict_for_neighbour = visited_nodes_to_statistics_set[neighbour]
+        minimal_solution_length = min(solution_len_dict_for_neighbour.keys())
+        list_of_minimal_solutions_for_neighbour = solution_len_dict_for_neighbour[minimal_solution_length]
+        minimal_solutions_per_neighbour.append(list_of_minimal_solutions_for_neighbour)
 
-        visited_nodes_to_statistics_set[node][selected_vertex] = min_selected_set
+    for possible_solution in powerset([*node]):  # all possible solutions are contained in powerset
+        if not is_valid_solution(possible_solution, node):
+            continue
+
+        if len(minimal_solutions_per_neighbour) > 0:
+            for permutation in permutations(minimal_solutions_per_neighbour):
+                combined_solution = set()
+                for v in possible_solution:
+                    combined_solution.add(v)
+
+                for neighbour_solution in permutation:
+                    for v in neighbour_solution:
+                        combined_solution.add(v)
+
+                solution_length = len(combined_solution)
+                if solution_length not in visited_nodes_to_statistics_set[node]:
+                    visited_nodes_to_statistics_set[node][solution_length] = []
+
+                visited_nodes_to_statistics_set[node][solution_length].append(combined_solution)
+
+        else:
+            solution_length = len(possible_solution)
+            if solution_length not in visited_nodes_to_statistics_set[node]:
+                visited_nodes_to_statistics_set[node][solution_length] = []
+
+            visited_nodes_to_statistics_set[node][solution_length].append(possible_solution)
 
 
 last_node_processed = None
@@ -114,11 +187,9 @@ while len(visited_nodes_to_statistics_set) < len(decomposition.nodes):
         break
 
 print("root node", last_node_processed)
-min_solution = None
 root_node_statistics = visited_nodes_to_statistics_set[last_node_processed]
-for root_node_vertex in root_node_statistics:
-    solution = root_node_statistics[root_node_vertex]
-    if min_solution is None or len(solution) < len(min_solution):
-        min_solution = solution
+min_solution_length = min(root_node_statistics.keys())
+min_solution = root_node_statistics[min_solution_length]
 
-print("min solution", min_solution)
+print("min solution:", min_solution)
+print("min solution length:", min_solution_length)
