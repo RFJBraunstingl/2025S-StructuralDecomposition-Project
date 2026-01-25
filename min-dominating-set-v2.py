ff42490c781f61a8d892ba7f77c03cf53abe4450
Roman Braunstingl <r.braunstingl@celix.at>
2026-01-25 08:58:34 +0100

    implement nice tree decomposition

--- min-dominating-set-v2.py
+++ min-dominating-set-v2.py
@@ -4,6 +4,7 @@ path_to_input = 'samples/balaban_10cage.gr'
 path_to_input = 'samples/star7.gr'
 path_to_input = 'samples/two-levels.gr'
 
+
 class Graph:
     adjacency_list = {}
 
@@ -21,6 +22,10 @@ class Graph:
     def create_edge(self, u, v):
         self.adjacency_list[u].append(v)
         self.adjacency_list[v].append(u)
+
+    def for_each_node_with_neighbours(self, consumer):
+        for node in self.adjacency_list:
+            consumer(node, adjacency_list[node])
 
     def copy(self):
         copy_of_adjacency_list = {}
@@ -44,10 +49,34 @@ class Graph:
 
         return node
 
+    def get_node_with_max_degree(self):
+        node = None
+        max_degree = 0
+        for v in self.adjacency_list:
+            if node is None:
+                node = v
+                max_degree = len(self.adjacency_list[v])
+                continue
+
+            if max_degree < len(self.adjacency_list[v]):
+                max_degree = len(self.adjacency_list[v])
+                node = v
+
+        return node
+
     def get_num_of_nodes(self):
         return len(self.adjacency_list)
 
-    def remove_node_and_fill_edges(self, node: int):
+    def remove_node(self, node):
+        neighbors = self.adjacency_list[node]
+        for i in range(len(neighbors)):
+            neighbour = neighbors[i]
+            # remove node to be popped from neighbour list
+            self.adjacency_list[neighbour].remove(node)
+
+        self.adjacency_list.pop(node)
+
+    def remove_node_and_fill_edges(self, node):
         neighbors = self.adjacency_list[node]
         for i in range(len(neighbors)):
             neighbour = neighbors[i]
@@ -165,3 +194,70 @@ for element in elimination_ordering:
 print("constructed tree decomposition...", tree_decomposition_graph)
 
 print("transforming tree composition into nice one...")
+# node types
+ROOT_NODE = "ROOT"
+JOIN_NODE = "JOIN"
+INTRODUCE_NODE = "INTRODUCE"
+FORGET_NODE = "FORGET"
+LEAF_NODE = "LEAF"
+
+class NiceTreeDecompositionNode:
+    node_type: str = ""
+    child_nodes = []
+    bag = frozenset()
+
+    def __str__(self):
+        return f"{self.node_type} {str(self.bag)}: [{', '.join([str(n) for n in self.child_nodes])}]"
+
+    def add_children(self, children, tree_decomposition_graph: Graph):
+        if len(children) == 0:
+            # add leaf node
+            leaf_node = NiceTreeDecompositionNode()
+            leaf_node.node_type = LEAF_NODE
+            self.child_nodes = [leaf_node]
+            return
+
+        # if we add more than 1 child, this node becomes a join node
+        if len(children) > 1:
+            half_index = len(children) // 2
+
+            child_1 = NiceTreeDecompositionNode()
+            child_1.bag = self.bag
+            child_1.add_children(children[:half_index], tree_decomposition_graph)
+            child_2 = NiceTreeDecompositionNode()
+            child_2.bag = self.bag
+            child_2.add_children(children[half_index:], tree_decomposition_graph)
+
+            self.node_type = JOIN_NODE
+            self.child_nodes = [child_1, child_2]
+
+        else:
+            bag = children[0]
+            neighbours = tree_decomposition_graph.get_neighbours(bag)
+            tree_decomposition_graph.remove_node(bag)
+
+            new_child = NiceTreeDecompositionNode()
+            new_child.bag = bag
+            new_child.add_children(neighbours, tree_decomposition_graph)
+            self.child_nodes = [new_child]
+
+
+class NiceTreeDecomposition:
+    root_node: NiceTreeDecompositionNode
+
+    @staticmethod
+    def create_from_tree_decomposition(tree_decomposition: Graph):
+        # input is a graph where every node is a bag
+        print("choose an arbitrary root node...")
+        root_node = NiceTreeDecompositionNode()
+        root_node.bag = frozenset()  # root has empty bag
+        root_node.node_type = ROOT_NODE
+
+        print("starting from root, insert all neighbours as children ensuring that the tree is binary...")
+        root_node.add_children([tree_decomposition_graph.get_node_with_max_degree()], tree_decomposition)
+
+        return root_node
+
+
+nice_tree_decomposition = NiceTreeDecomposition.create_from_tree_decomposition(tree_decomposition_graph)
+print(nice_tree_decomposition)
